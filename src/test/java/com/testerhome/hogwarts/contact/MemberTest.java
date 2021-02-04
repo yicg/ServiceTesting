ba6a11e921f4ada63fbe8e04665c4c174a856ed1
yicg <yichunguangyx@163.com>
2021-02-04 22:14:34 +0800

    junit5的数据驱动,驱动csv文件

--- src/test/java/com/testerhome/hogwarts/contact/MemberTest.java
+++ src/test/java/com/testerhome/hogwarts/contact/MemberTest.java
@@ -2,6 +2,7 @@ package com.testerhome.hogwarts.contact;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
@@ -35,6 +36,24 @@ class MemberTest {
         HashMap<String,Object> map=new HashMap<>();
         map.put("userid",nameNew);
         map.put("name",nameNew);
+        map.put("department", Arrays.asList(1,2));
+        map.put("mobile","153"+random.substring(0,8));
+        map.put("email",random+"@qq.com");
+
+        member.createMember(map).then().log().all().statusCode(200).body("errcode",equalTo(0));
+    }
+
+    /**
+     * junit5的数据驱动,驱动csv文件
+     */
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data/members.csv")
+    void createMember(String name,String alisa) {
+        String nameNew=name+random;
+        HashMap<String,Object> map=new HashMap<>();
+        map.put("userid",nameNew);
+        map.put("name",nameNew);
+        map.put("alias",alisa);
         map.put("department", Arrays.asList(1,2));
         map.put("mobile","153"+random.substring(0,8));
         map.put("email",random+"@qq.com");
